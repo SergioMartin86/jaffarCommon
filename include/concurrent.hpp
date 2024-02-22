4aef16b83e6e1c7e471b0e5b05a69ab59ecda98b
SergioMartin86 <eienburuu@gmail.com>
2024-02-22 17:12:33 +0000

    Licensing and a small fix to the concurrent multimap template

--- include/concurrent.hpp
+++ include/concurrent.hpp
@@ -17,7 +17,7 @@ namespace jaffarCommon
 template <class T> using atomicQueue_t = atomic_queue::AtomicQueueB<T>;
 template <class V> using HashSet_t = phmap::parallel_flat_hash_set<V, phmap::priv::hash_default_hash<V>, phmap::priv::hash_default_eq<V>, std::allocator<V>, 4, std::mutex>;
 template <class K, class V> using HashMap_t = phmap::parallel_flat_hash_map<K, V, phmap::priv::hash_default_hash<K>, phmap::priv::hash_default_eq<K>, std::allocator<std::pair<const K, V>>, 4, std::mutex>;
-template <class K, class V> using concurrentMultimap_t = oneapi::tbb::concurrent_multimap<K,V, std::greater<K>>; 
+template <class K, class V, class C = std::greater<K>> using concurrentMultimap_t = oneapi::tbb::concurrent_multimap<K,V,C>;
 
 template <class T> class concurrentDeque
 {
