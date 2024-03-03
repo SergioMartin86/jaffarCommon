7417d6b1638a6490174df36b70319be1d9beb87d
SergioMartin86 <eienburuu@gmail.com>
2024-03-03 19:51:00 +0100

    Adding file tests

---
+++ tests/file.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <cstdio> 
+#include "gtest/gtest.h"
+#include "../include/file.hpp"
+
+using namespace jaffarCommon::file;
+
+TEST(file, loadFile)
+{
+  std::string fileName = "testFile1.txt";
+  std::string expectedContents = "Hello World!";
+
+  std::string fileContents = "";
+  ASSERT_FALSE(loadStringFromFile(fileContents, "WrongPath.txt"));
+  ASSERT_TRUE(loadStringFromFile(fileContents, fileName));
+  ASSERT_EQ(fileContents, expectedContents);
+}
+
+
+TEST(file, saveFile)
+{
+  std::string fileName = "testFile2.txt";
+  std::string expectedContents = "Hello World!";
+  ASSERT_FALSE(saveStringToFile(expectedContents, "/dev/null/foo"));
+  ASSERT_TRUE(saveStringToFile(expectedContents, fileName));
+
+  std::string fileContents;
+  std::ifstream t;
+  t.open(fileName);
+  std::getline(t, fileContents);
+  t.close();
+
+  ASSERT_EQ(expectedContents, fileContents);
+}
